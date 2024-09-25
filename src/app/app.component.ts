5060072e100985749d146336a0f1a07b03e83d32
Eduardo <dudu_moreira9@hotmail.com>
2024-09-25 20:39:22 -0300

    feat: create ucp grid structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ClipboardModule],
+  imports: [RouterOutlet, ClipboardModule, SidebarComponent, CommonModule],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ExperienceLife';
+
+  constructor(private router: Router) {
+
+  }
+
+  isUcpRoute(): boolean {
+    return this.router.url.includes('ucp');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UserProfileComponent } from './pages/ucp/user-profile/user-profile.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    { path: '', component: HomeComponent },
+    { path: 'ucp', component: UserProfileComponent },
 ];
