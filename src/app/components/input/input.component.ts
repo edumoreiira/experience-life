ae62cab952cf2fcde8340d3c18daff221811bd29
Eduardo <dudu_moreira9@hotmail.com>
2024-10-25 11:14:54 -0300

    feat(input): add acessibility and placeholder to input component

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, forwardRef, input, viewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, forwardRef, input, viewChild } from '@angular/core';
 import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { CheckInput } from '../../models/check-input.interface';
 import { fadeInOut } from '../../animations/transition-animations';
@@ -25,13 +25,21 @@ type InputTypes = "text" | "email" | "password" | "date" | "search" | "tel" | "n
 })
 
 
-export class InputComponent implements ControlValueAccessor{
+export class InputComponent implements ControlValueAccessor, OnInit {
   @Input() type: InputTypes = "text";
+  @Input() title: string = "";
   @Input() placeholder: string = "";
   @Input() formControlName: string = '';
   @Input() isInvalid: boolean = false;
   @Input() rightInfo: boolean = false;
   @Output() inputElement = new EventEmitter<CheckInput>;
+  @Input({ required: true }) id = '';
+
+  //accessibility
+  @Input() ariaLabel: string = '';
+  @Input() ariaLabelledby: string = '';
+  @Input() ariaRequired: boolean = false;
+  
 
   //ngxmask inputs
   @Input() dropSpecialCharacters: boolean = false;
@@ -43,15 +51,31 @@ export class InputComponent implements ControlValueAccessor{
 
 
   hideShowPassword = input<boolean>(false);
-  showPassword: boolean = false;
+  passwordVisible: boolean = false;
+  inputErrorId: string = '';
   value: string = "";
 
   constructor(private element: ElementRef) { }
 
-  hasInvalidClass() {
+  ngOnInit(): void {
+    this.inputErrorId = "input-error-" + this.id;
+  }
+
+  get hasInvalidClass() {
     return this.element.nativeElement.classList.contains('ng-invalid') && this.element.nativeElement.classList.contains('ng-dirty');
   }
 
+  showPassword() {
+    this.passwordVisible = false;
+  }
+
+  hidePassword() {
+    this.passwordVisible = true;
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
   
 
   //NGVALUEACCESSOR
