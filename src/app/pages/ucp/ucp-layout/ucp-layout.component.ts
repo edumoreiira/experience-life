bb8ecdd1b00853b5c5a25b5f3586a70eaa2be377
Eduardo <dudu_moreira9@hotmail.com>
2024-10-26 16:34:23 -0300

    feat(ucp-layout): add logout confirmation dialog

--- src/app/pages/ucp/ucp-layout/ucp-layout.component.ts
+++ src/app/pages/ucp/ucp-layout/ucp-layout.component.ts
@@ -1,16 +1,22 @@
 import { Component, HostBinding } from '@angular/core';
 import { SidebarComponent, SidebarItem } from '../../../components/sidebar/sidebar.component';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { ModalComponent } from "../../../components/modal/modal.component";
+import { ConfirmationDialogComponent } from "../../../components/confirmation-dialog/confirmation-dialog.component";
 
 @Component({
   selector: 'app-ucp-layout',
   standalone: true,
-  imports: [SidebarComponent, RouterOutlet, RouterModule],
+  imports: [SidebarComponent, RouterOutlet, RouterModule, ModalComponent, ConfirmationDialogComponent],
   templateUrl: './ucp-layout.component.html',
   styleUrl: './ucp-layout.component.scss'
 })
 export class UcpLayoutComponent {
   version= '1.0.0';
+  changePasswordModal: boolean = false;
+  logoutConfirmationDialog: boolean = false;
+
+  constructor( private router: Router) { }
 
   navbarItems: SidebarItem[] = [
     {
@@ -67,11 +73,12 @@ export class UcpLayoutComponent {
       options: [
         {
           title: 'Alterar senha',
-          route: ''
+          route: '',
+          onClick: () => this.openChangePasswordModal()
         },
         {
           title: 'Sair',
-          route: ''
+          onClick: () => this.openLogoutConfirmationDialog(),
         },
       ]
     },
@@ -79,6 +86,30 @@ export class UcpLayoutComponent {
 
   @HostBinding('class.ucp_c') get ucpContainer() {
     return true;
+  }
+
+  openChangePasswordModal() {
+    this.changePasswordModal = true;
+  }
+
+  closeChangePasswordModal() {
+    this.changePasswordModal = false;
+  }
+
+  openLogoutConfirmationDialog() {
+    this.logoutConfirmationDialog = true;
+  }
+
+  closeLogoutConfirmationDialog() {
+    this.logoutConfirmationDialog = false;
+  }
+
+  onLogoutConfirmation(confirm: boolean) {
+    if(confirm) {
+      this.router.navigate(['/']);
+      // handle logout
+    }
+    this.closeLogoutConfirmationDialog();
   }
 
 }
